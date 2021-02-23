76ac4d5f2d82d8aa8ce409cb23a9a77e4bfec452
WinEd73 <edwinyankov2000@gmail.com>
2021-02-22 21:16:55 -0600

    first commit

---
+++ src/uaslp/ingenieria/labs/list/LinkedList.java
@@ -0,0 +1,16 @@
+package uaslp.ingenieria.labs.list;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+}
